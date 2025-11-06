f14b618ddfa5c9539a92a53581ac01cee59cd48e
r-kaveri24 <rautkaver@gmail.com>
2025-11-06 19:51:37 +0530

    changes whish list to wish list

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -320,7 +320,7 @@ const TheYardHeist = () => {
 
               <Button size="lg" variant="outline" className="paint-splatter-green text-lg px-8">
                 <Lock className="w-5 h-5 mr-2" />
-                Join Beta (Whishlist)
+                Join Beta (Wishlist)
               </Button>
               <Dialog>
                 <DialogTrigger asChild>
